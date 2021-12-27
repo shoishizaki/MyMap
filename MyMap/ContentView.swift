7b9ca3ae91d5e87a8fc05170e7ef764f0e77e03a
shoishizaki <shoishizaki1997@gmail.com>
2021-12-28 05:51:35 +0900

    Create MAP app

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -1,9 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var inputText: String = ""
+    
+    @State var dispSearchKey: String = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("キーワードを入力してください",
+                      text: $inputText, onCommit: {
+                dispSearchKey = inputText
+                
+                print("入力したキーワード:" + dispSearchKey)
+            })
+                .padding()
+            
+            MapView(searchKey: dispSearchKey)
+            
+        }
+        
     }
 }
 
