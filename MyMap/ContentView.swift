c38ab275ce6644499654866644741690b050de1f
shoishizaki <shoishizaki1997@gmail.com>
2021-12-28 05:37:30 +0900

    Create MapView

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  MyMap
-//
-//  Created by 石崎將 on 2021/12/28.
-//
-
 import SwiftUI
 
 struct ContentView: View {
---
+++ MyMap/MapView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    let searchKey: String
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        print(searchKey)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(
+            searchKey,
+            completionHandler: { (placemarks,error) in
+            if let unwrapPlacemarks = placemarks,
+               let firstPlacemark = unwrapPlacemarks.first,
+               let location = firstPlacemark.location {
+                
+                let targetCoordinate = location.coordinate
+                
+                print(targetCoordinate)
+                
+                let pin = MKPointAnnotation()
+                
+                pin.coordinate = targetCoordinate
+                
+                pin.title = searchKey
+                
+                uiView.addAnnotation(pin)
+                
+                uiView.region = MKCoordinateRegion(
+                center: targetCoordinate,
+                latitudinalMeters: 500.0,
+                longitudinalMeters:  500.0)
+            }
+        })
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(searchKey: "東京タワー")
+    }
+}
