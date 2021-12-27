bbc804db040784f972a7880943609fa61e2a94ec
shoishizaki <shoishizaki1997@gmail.com>
2021-12-28 04:41:36 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 石崎將 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
